5aea8d804e314af4fe28e776664a0cecf18a219f
suporte3wings <suporte@3wings.com.br>
2024-09-25 08:00:10 -0300

    feat: :sparkles: add paralelism and condition

--- processor/ingestion_test_processor.py
+++ processor/ingestion_test_processor.py
@@ -17,28 +17,33 @@ class IngestionTestProcessor:
         description="Flow to validate new ingestion table"
     )
     def ingestion_test_process(self):
-        self._start_flow()
-        self._upload_file_test.submit()
-        self._change_params_ingestion.submit()
-        self._check_transfer_files_between_bucket()
+        result_start_flow = self._start_flow()
+        result_upload = self._upload_file_test.submit(result_start_flow)
+        result_change_params = self._change_params_ingestion.submit(result_start_flow)
+        self._check_transfer_files_between_bucket.submit(
+            result_upload, result_change_params
+        )
 
     @task(name="start_flow")
     def _start_flow(self):
         sleep(5)
+        return True
 
     @task(name="upload_file_test")
-    def _upload_file_test(self):
+    def _upload_file_test(self, _dep):
         self._s3_service.upload_file_s3(
             self._params.filepath,
             self._build_filename_s3()
         )
+        return True
 
     @task(name="change_params_ingestion")
-    def _change_params_ingestion(self):
+    def _change_params_ingestion(self, _dep):
         sleep(10)
+        return True
 
     @task(name="check_transfer_files_between_bucket")
-    def _check_transfer_files_between_bucket(self):
+    def _check_transfer_files_between_bucket(self, _dep1, _dep2):
         sleep(5)
 
     def _build_filename_s3(self):
