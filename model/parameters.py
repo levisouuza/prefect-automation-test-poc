3b1a7642b4d50deb820f78152f9bf1bb8017cbb7
levisouuza <levisouuza@gmail.com>
2024-09-28 21:17:42 -0300

    refactor: :recycle: add new params lambda_name_start_task, lambda_name_start_job, database_sor

--- model/parameters.py
+++ model/parameters.py
@@ -3,11 +3,15 @@ from pydantic import BaseModel
 
 
 class Parameters(BaseModel):
-    external_bucket: str
-    stage_bucket: str
+    bucket_external: str
+    bucket_stage: str
+    bucket_sor: str
     filepath: Optional[str] = None
     provider: str
     business: str
-    lambda_name: Optional[str] = None
+    lambda_name_start_task: Optional[str] = None
+    lambda_name_start_job: Optional[str] = None
+    database_sor: str
+    table_name: str
 
 
